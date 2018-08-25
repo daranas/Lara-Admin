7206b74d8238e6384641e6ab87dd5cc5072b1e71
DaranaSV <darana.sv@gmail.com>
2018-08-25 16:35:09 +0700

    change default style

--- app/Http/admin_routes.php
+++ app/Http/admin_routes.php
@@ -1,8 +1,6 @@
 <?php
 
-/* ================== Homepage ================== */
-Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
+/* ================== Auth ================== */
 Route::auth();
 
 /* ================== Access Uploaded Files ================== */
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,82 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
     <head>
-        <title>Laravel</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <title>Lara Admin</title>
 
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
         <style>
             html, body {
-                height: 100%;
-            }
-
-            body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
                 margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
             }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
             }
 
             .content {
                 text-align: center;
-                display: inline-block;
             }
 
             .title {
-                font-size: 96px;
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="flex-center position-ref full-height">
+            <div class="top-right links">
+                @if (Auth::guest())
+                    <a href="{{ url('/public/login') }}">Login</a>
+                @else
+                    <a href="{{ url(config('laraadmin.adminRoute')) }}">{{ Auth::user()->name }}</a>
+                @endif
+            </div>
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title m-b-md">
+                    Lara Admin
+                </div>
             </div>
         </div>
     </body>
